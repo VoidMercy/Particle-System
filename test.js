9d211ac065fdbd788648e2379d3c86b073b96910
Alex Lin <lin969@purdue.edu>
2019-10-22 00:18:56 -0400

    random snowflake movement

--- test.js
+++ test.js
@@ -10,22 +10,29 @@ PIXI.Loader.shared.add(snowflake_url).load(setup);
 
 var snowflakes = [];
 
+var VELOCITY = 3;
+var SNOW_RATE = 0.15;
+
 function setup() {
   app.ticker.add(delta => gameLoop(delta));
 }
 
 function gameLoop(delta) {
   mouse_coord = app.renderer.plugins.interaction.mouse.global;
-  if (Math.random() < 0.1) {
+  if (Math.random() < 0.15) {
     var temp = new PIXI.Sprite(PIXI.Loader.shared.resources[snowflake_url].texture);
     temp.x = mouse_coord.x;
     temp.y = mouse_coord.y;
+    temp.theta = Math.random() * Math.PI / 3 - 2 * Math.PI / 3;
+    console.log(temp.theta);
     temp.scale.x = 0.1;
     temp.scale.y = 0.1;
     snowflakes.push(temp);
     app.stage.addChild(temp);
   }
   for (var i = 0; i < snowflakes.length; i++) {
-    snowflakes[i].y += 1;
+    var sf = snowflakes[i];
+    sf.x += Math.cos(sf.theta) * VELOCITY;
+    sf.y -= Math.sin(sf.theta) * VELOCITY;
   }
 }
