cac158849acac85a31982201a89136aa69eb39e5
Alex Lin <lin969@purdue.edu>
2019-10-26 11:04:17 -0400

    add play pause functionality

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 // Initializations
 let WIDTH = 800;
 let HEIGHT = 600;
+let running = true;
 
 button_url = "http://localhost:8000/button.png";
 snowflake_url = "http://localhost:8000/snowflake.png";
 raindrop_url  = "http://localhost:8000/raindrop.png";
 bee_url  = "http://localhost:8000/bee.png";
-image_urls = [button_url, snowflake_url, raindrop_url, bee_url];
+play_url  = "http://localhost:8000/play.png";
+pause_url  = "http://localhost:8000/pause.png";
+image_urls = [button_url, snowflake_url, raindrop_url, bee_url, play_url, pause_url];
 
 simulation_status = {
   "snowflake":{running: false, run: add_snowflake, button: snowflake_url},
@@ -34,69 +37,73 @@ PIXI.Loader.shared.load(setup);
 // Setup
 function setup() {
   create_menu();
-  app.ticker.add(delta => game_loop(delta));
+  app.ticker.add(delta => main_loop(delta));
 }
 
-function game_loop() {
-  let gr = game_map;
-  stage.removeChild(game_map);
-  gr.clear();
+function main_loop(delta) {
+  if (running) {
+    let gr = game_map;
+    stage.removeChild(game_map);
+    gr.clear();
 
-  for (let type in simulation_status) {
-    if (simulation_status[type].running) {
-      let new_particle = simulation_status[type].run();
-      if (new_particle !== 0) {
-        particles.push(new_particle);
+    for (let type in simulation_status) {
+      if (simulation_status[type].running) {
+        let new_particle = simulation_status[type].run();
+        if (new_particle !== 0) {
+          particles.push(new_particle);
+        }
       }
     }
-  }
 
-  // Update particles by calling their update and draw functions
-  for (let i = particles.length - 1; i >= 0; i--) {
-    particles[i].update(particles[i]);
-    particles[i].draw(particles[i], gr);
-    if (particles[i].offscreen(particles[i])) {
-      stage.removeChild(particles[i]);
-      particles.splice(i, 1);
+    // Update particles by calling their update and draw functions
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update(particles[i]);
+      particles[i].draw(particles[i], gr);
+      if (particles[i].offscreen(particles[i])) {
+        stage.removeChild(particles[i]);
+        particles.splice(i, 1);
+      }
     }
-  }
 
-  stage.addChild(gr);
+    stage.addChild(gr);
+  }
 }
 
 // Create simulation buttons
 function create_menu() {
+  let PLAY_PAUSE_SIZE = 50;
   let BUTTON_WIDTH = 80;
   let BUTTON_HEIGHT = 80;
-  let BUTTON_IMG_SIZE = 30;
-  let BUTTON_IMG_WIDTH = 30;
-  let BUTTON_IMG_HEIGHT = 30;
+  let BUTTON_ICON_SIZE = 30;
+  let BUTTON_ICON_WIDTH = 30;
+  let BUTTON_ICON_HEIGHT = 30;
 
   var c = 0;
 
   for (let type in simulation_status) {
     // Create button and scale image
-    let button_img_url = simulation_status[type].button;
+    let button_icon_url = simulation_status[type].button;
     let button = new PIXI.Sprite(PIXI.Loader.shared.resources[button_url].texture);
     button.scale.x = BUTTON_WIDTH / button.width;
     button.scale.y = BUTTON_HEIGHT / button.height;
     button.x = WIDTH - button.width;
     button.y = c * button.height;
 
-    let button_img = new PIXI.Sprite(PIXI.Loader.shared.resources[button_img_url].texture);
+    let button_icon = new PIXI.Sprite(PIXI.Loader.shared.resources[button_icon_url].texture);
 
-    if (button_img.width > button_img.height) {
-      BUTTON_IMG_WIDTH = BUTTON_IMG_SIZE;
-      BUTTON_IMG_HEIGHT = BUTTON_IMG_SIZE * button_img.height / button_img.width;
+    // maintain button icon aspect ratio
+    if (button_icon.width > button_icon.height) {
+      BUTTON_ICON_WIDTH = BUTTON_ICON_SIZE;
+      BUTTON_ICON_HEIGHT = BUTTON_ICON_SIZE * button_icon.height / button_icon.width;
     } else {
-      BUTTON_IMG_HEIGHT = BUTTON_IMG_SIZE;
-      BUTTON_IMG_WIDTH = BUTTON_IMG_SIZE * button_img.width / button_img.height;
+      BUTTON_ICON_HEIGHT = BUTTON_ICON_SIZE;
+      BUTTON_ICON_WIDTH = BUTTON_ICON_SIZE * button_icon.width / button_icon.height;
     }
 
-    button_img.scale.x = BUTTON_IMG_WIDTH / button_img.width;
-    button_img.scale.y = BUTTON_IMG_HEIGHT / button_img.height;
-    button_img.x = WIDTH - BUTTON_WIDTH + (BUTTON_WIDTH - BUTTON_IMG_WIDTH) / 2;
-    button_img.y = c * button.height + (BUTTON_HEIGHT - BUTTON_IMG_HEIGHT) / 2;
+    button_icon.scale.x = BUTTON_ICON_WIDTH / button_icon.width;
+    button_icon.scale.y = BUTTON_ICON_HEIGHT / button_icon.height;
+    button_icon.x = WIDTH - BUTTON_WIDTH + (BUTTON_WIDTH - BUTTON_ICON_WIDTH) / 2;
+    button_icon.y = c * button.height + (BUTTON_HEIGHT - BUTTON_ICON_HEIGHT) / 2;
 
     // Add button listener events
     button.interactive = true;
@@ -105,9 +112,35 @@ function create_menu() {
       console.log(type);
     });
     stage.addChild(button);
-    stage.addChild(button_img);
+    stage.addChild(button_icon);
 
     c += 1;
   }
+
+  // create pause and play buttons
+  button = new PIXI.Sprite(PIXI.Loader.shared.resources[play_url].texture);
+  button.scale.x = PLAY_PAUSE_SIZE / button.width;
+  button.scale.y = PLAY_PAUSE_SIZE / button.height;
+  button.x = WIDTH / 2 - button.width / 2;
+  button.y = 0;
+  button.interactive = true;
+  button.on("click", function() {
+    running = true;
+  });
+
+  stage.addChild(button);
+
+  button = new PIXI.Sprite(PIXI.Loader.shared.resources[pause_url].texture);
+  button.scale.x = PLAY_PAUSE_SIZE / button.width;
+  button.scale.y = PLAY_PAUSE_SIZE / button.height;
+  button.x = WIDTH / 2 + button.width / 2;
+  button.y = 0;
+  button.interactive = true;
+  button.on("click", function() {
+    running = false;
+  });
+
+  stage.addChild(button);
+
 }
 
